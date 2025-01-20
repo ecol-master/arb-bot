7b6dd91753546f813a911a2c347b291f45113575
Dmitry Kuzmin <dmitrykuzmin00@gmail.com>
2025-01-20 12:51:45 +0300

    chore: add subscription for pedinging tx in mempool

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,10 @@ use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 use alloy::{
+    consensus::Transaction,
     dyn_abi::abi::token,
-    primitives::{address, U256},
-    providers::{Provider, ProviderBuilder, WsConnect},
+    primitives::{address, TxNumber, U256},
+    providers::{self, Provider, ProviderBuilder, WsConnect},
     sol,
 };
 use futures_util::{stream, StreamExt};
@@ -30,46 +31,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::init_from_env()?;
     info!("Started listen rpc: {}", config.rpc_url);
 
-    let provider = ProviderBuilder::new()
-        .on_ws(WsConnect::new(config.rpc_url))
-        .await?;
-    let provider = Arc::new(provider);
+    let provider = Arc::new(
+        ProviderBuilder::new()
+            .on_ws(WsConnect::new(config.rpc_url))
+            .await?,
+    );
 
-    let eth_usdc_adr = address!("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640");
-    let eth_usdc = IUniswapV3Pool::new(eth_usdc_adr, provider.clone());
+    let mut stream = provider
+        .subscribe_pending_transactions()
+        .await?
+        .into_stream(); // Wait and take the next 3 transactions.
+    println!("Awaiting pending transactions...");
 
-    // ETH/USDC Pool returns as token0 - USDC address, and token1 - ETH
-    let usdc_adr = eth_usdc.token0().call().await?._0;
-    let usdc = IERC20::new(usdc_adr, provider.clone());
-    let mut usdc_supply = usdc.totalSupply().call().await?._0;
+    let router02_adr = address!("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+    let router03_adr = address!("0xE592427A0AEce92De3Edee1F18E0157C05861564");
+    // Take the stream and print the pending transaction.
+    let handle = tokio::spawn(async move {
+        while let Some(tx_hash) = stream.next().await {
+            // Get the transaction details.
+            let tx = match provider.get_transaction_by_hash(tx_hash).await {
+                Ok(tx) => match tx {
+                    Some(tx) => tx,
+                    None => continue,
+                },
+                _ => continue,
+            };
 
-    let eth_adr = eth_usdc.token1().call().await?._0;
-    let eth = IERC20::new(eth_adr, provider.clone());
-    let mut eth_supply = eth.totalSupply().call().await?._0;
+            if let Some(to) = tx.to() {
+                if to == router02_adr {
+                    info!("Router02: {:?}", tx.to());
+                }
+                if to == router03_adr {
+                    info!("Router03: {:?}", tx.to());
+                }
+            }
+        }
+    });
 
-    let subscription = provider.subscribe_blocks().await?;
-    let mut stream = subscription.into_stream();
+    handle.await?;
 
-    while let Some(block) = stream.next().await {
-        info!("NEW BLOCK: {}", block.number);
-
-        let usdc_current_supply = usdc.totalSupply().call().await?._0;
-        let usdc_diff = usdc_current_supply.abs_diff(usdc_supply);
-        info!("USDC supply diff: {}", usdc_diff);
-
-        let eth_current_supply = eth.totalSupply().call().await?._0;
-        let eth_diff = eth_current_supply.abs_diff(eth_supply);
-        info!("ETH supply diff: {}", eth_diff);
-
-        let precision: u32 = 6;
-        let usdc_price_diff = (usdc_diff * U256::from(10u32.pow(precision + 2))) / usdc_supply;
-        let eth_price_diff = (eth_diff * U256::from(10u32.pow(precision + 2))) / eth_supply;
-        info!("USDC price diff(%): {} / 10^{}", usdc_price_diff, precision);
-        info!("ETH price diff(%): {} / 10^{}", eth_price_diff, precision);
-
-        usdc_supply = usdc_current_supply;
-        eth_supply = eth_current_supply;
-    }
+    //while let Some(tx) = provider.sub
 
     Ok(())
 }
